fbe738ebb0da15f065c6973c12e20cf5ab06f97a
alizadeh-mohsen <kelarens@gmail.com>
2023-01-18 19:50:30 +0330

    Adding AccountController and register dto

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext dataContext;
+
+        public AccountController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        {
+            if (await userExists(registerDto.Username))
+                return BadRequest("Username is taken");
+
+            using var hmac = new HMACSHA512();
+            var user = new AppUser
+            {
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            dataContext.Users.Add(user);
+            await dataContext.SaveChangesAsync();
+            return user;
+        }
+
+
+        private async Task<bool> userExists(string username)
+        {
+            return await dataContext.Users.AnyAsync(u => u.UserName == username.ToLower());
+        }
+    }
+}
