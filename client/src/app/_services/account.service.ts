2b318bd7095d8108c2969c4ea7fdc3bd1571c03e
alizadeh-mohsen <kelarens@gmail.com>
2023-01-20 14:25:13 +0330

    adding login

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,16 +1,37 @@
-import { HttpClient } from '@angular/common/http';
+import { User } from './../_models/User';
+import { HttpClient, JsonpInterceptor } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { BehaviorSubject, map } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+
+  private currentUsersource = new BehaviorSubject<User | null>(null);
+  currentUser$ = this.currentUsersource.asObservable();
 
   baseUrl = 'http://localhost:5001/api/';
 
   constructor(private http: HttpClient) { }
 
   login(model: any) {
-    return this.http.post(this.baseUrl + 'account/login', model);
+    return this.http.post<User>(this.baseUrl + 'account/login', model)
+      .pipe(
+        map((response: User) => {
+          const user = response;
+          if (user)
+            localStorage.setItem('user', JSON.stringify(user));
+          this.currentUsersource.next(user);
+        })
+      );
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUsersource.next(null);
+  }
+
+  setCurrentUser(user: User) {
+    this.currentUsersource.next(user);
   }
 }
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AccountService } from './_services/account.service';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
@@ -9,18 +10,33 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   title = 'Dating App';
-  users: any ;
+  users: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private accountService: AccountService) { }
 
   ngOnInit() {
-   
+    this.getUsers();
+    this.setUser();
+
+  }
+
+  getUsers() {
     this.http.get('http://localhost:5001/api/Users').subscribe({
-      next: response => {this.users = response;console.log(response);
+      next: response => {
+        this.users = response;
+        console.log(response);
       },
       error: error => console.log(error.error),
       complete: () => console.log('request completed')
     });
   }
 
+  setUser() {
+    const user = localStorage.getItem('user');
+    if (user)
+      this.accountService.setCurrentUser(JSON.parse(user));
+    else
+      return;
+
+  }
 }
