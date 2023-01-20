379f503c37efa2e5979fa78ca588a05cb0ff4f40
alizadeh-mohsen <kelarens@gmail.com>
2023-01-20 22:30:39 +0330

    adding theme and sharedmodule

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -9,10 +9,16 @@ import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "members", component: MemberListComponent, canActivate: [AuthGuard] },
-  { path: "members/:id", component: MemberDetailComponent, canActivate: [AuthGuard] },
-  { path: "lists", component: ListsComponent, canActivate: [AuthGuard] },
-  { path: "messages", component: MessagesComponent, canActivate: [AuthGuard] },
+  {
+    path: "",
+    runGuardsAndResolvers:'always',
+    canActivate: [AuthGuard],
+    children: [
+    { path: "members", component: MemberListComponent, canActivate: [AuthGuard] },
+    { path: "members/:id", component: MemberDetailComponent, canActivate: [AuthGuard] },
+    { path: "lists", component: ListsComponent, canActivate: [AuthGuard] },
+    { path: "messages", component: MessagesComponent, canActivate: [AuthGuard] },]
+  },
   { path: "**", component: MessagesComponent, pathMatch: 'full' },
 ];
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,18 @@
+import { SharedModule } from './_modules/shared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
-import { ToastrModule } from 'ngx-toastr';
+
 
 @NgModule({
   declarations: [
@@ -32,11 +30,8 @@ import { ToastrModule } from 'ngx-toastr';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    BrowserAnimationsModule,
-    BsDropdownModule.forRoot(),
-    ToastrModule.forRoot({
-      positionClass:'toast-bottom-right'
-    })
+    SharedModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
