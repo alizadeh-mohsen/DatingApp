a94ad60786f6e751f2d7646ef6b16e86ee7edaa9
alizadeh-mohsen <kelarens@gmail.com>
2023-01-31 20:43:08 +0330

    Reactiveform

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { SharedModule } from './_modules/shared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -43,6 +43,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     SharedModule
 
   ],
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -2,6 +2,8 @@ import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
 import { AccountService } from './../_services/account.service';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -11,15 +13,27 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   @Output() cancelRegister = new EventEmitter();
+  registerForm: FormGroup = new FormGroup({});
 
   model: any = {}
 
   constructor(private accountService: AccountService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl()
+    });
   }
 
   register() {
+    console.log(this.registerForm.value);
+    
     this.accountService.register(this.model).subscribe({
       next: _ => this.router.navigateByUrl('/')
     });
